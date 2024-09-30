6995ef4ccf1a207400a3e1dd7fb51a08fc53272b
Krupamaisuria <Krupamaisuria94@gmail.com>
2024-09-30 01:32:12 -0400

    signup successfully done

---
+++ SocialTrailsApp/Preview Content/Users.swift
@@ -0,0 +1,36 @@
+//
+//  Users.swift
+//  SocialTrailsApp
+//
+//  Created by Admin on 9/29/24.
+//
+
+import Foundation
+struct Users {
+    var userId : String
+    var username: String
+    var email: String
+    var createdOn = Date().timeIntervalSince1970
+    var profilepicture: String?
+    var roles: String
+    var profiledeleted=false
+    var notification = true
+    var admindeleted = false
+    var suspended = false
+    var isActive = true
+    
+    func toDictionary() -> [String: Any] {
+           return [
+               "userId": userId,
+               "username": username,
+               "email": email,
+               "createdOn": createdOn,
+               "roles": roles,
+               "profiledeleted": profiledeleted,
+               "notification": notification,
+               "admindeleted": admindeleted,
+               "suspended": suspended,
+               "isActive": isActive as Any
+           ]
+       }
+}
