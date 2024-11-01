9d8dd9362539ba67ec8befa87174253b90de56fc
Barsha Roka Magar <barsharoka2626@gmail.com>
2024-11-01 12:27:18 -0400

    adminreport is done

---
+++ SocialTrailsApp/Preview Content/AdminPanel/AdminReportListView.swift
@@ -0,0 +1,112 @@
+import SwiftUI
+
+struct AdminReportListView: View {
+    @StateObject private var reportService = ReportService()
+    @State private var reportsWithUserInfo: [Report] = []
+    @State private var selectedReportId: String?
+    @State private var isPostDetailActive = false
+    @State private var isUserProfileActive = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                List(reportsWithUserInfo, id: \.reportId) { report in
+                    reportRow(report: report)
+                }
+                .onAppear(perform: fetchReports)
+                .navigationTitle("Admin Reports")
+                .font(.title)
+                
+                // Detail Section
+            }
+            .background(
+                NavigationLink(
+                    destination: AdminPostDetailView(postDetailId: selectedReportId ?? ""),
+                    isActive: $isPostDetailActive
+                ) {
+                    EmptyView()
+                }
+            )
+            .background(
+                NavigationLink(
+                    destination: AdminUserManageView(userId: selectedReportId ?? ""),
+                    isActive: $isUserProfileActive
+                ) {
+                    EmptyView()
+                }
+            )
+        }
+    }
+
+    private func reportRow(report: Report) -> some View {
+        HStack {
+            // Profile Image from report (if available)
+            if let profilePictureURL = report.userprofilepicture, !profilePictureURL.isEmpty,
+               let url = URL(string: profilePictureURL) {
+                AsyncImage(url: url) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                        .padding(.trailing, 8)
+                } placeholder: {
+                    ProgressView()
+                }
+            } else {
+                // Default person icon if no profile picture
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(.gray)
+                    .clipShape(Circle())
+                    .padding(.trailing, 8)
+            }
+           
+            VStack(alignment: .leading) {
+                Text(report.username ?? "Unknown") // Display username, if available
+                    .font(.headline)
+                Text(report.createdon)
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("Type: \(report.reporttype)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                Text("Status: \(report.status.rawValue)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+            
+            // Eye icon to select the report
+            Button(action: {
+                self.selectedReportId = report.reportedId
+                if report.reporttype == ReportType.post.rawValue {
+                    self.isPostDetailActive = true
+                } else if report.reporttype == ReportType.user.rawValue {
+                    self.isUserProfileActive = true
+                } else {
+                    // Show a message if the report type is invalid
+                    print("No valid action for this report type")
+                }
+            }) {
+                Image(systemName: "eye")
+                    .foregroundColor(.blue)
+            }
+        }
+    }
+    
+    private func fetchReports() {
+        reportService.fetchReports { result in
+            switch result {
+            case .success(let fetchedReports):
+                self.reportsWithUserInfo = fetchedReports
+            case .failure(let error):
+                // Handle the error (e.g., show an alert)
+                print("Error fetching reports: \(error)")
+            }
+        }
+    }
+}
--- SocialTrailsApp/Utility/ReportService.swift
+++ SocialTrailsApp/Utility/ReportService.swift
@@ -11,12 +11,11 @@ import FirebaseDatabase
 class ReportService: ObservableObject {
     private var reference: DatabaseReference
     private let collectionName = "report"
-    
-    @Published var reportCount: Int = 0
+    private let userService = UserService()
+   
 
     init() {
         self.reference = Database.database().reference()
-       
     }
 
     func addReport(data: Report, completion: @escaping (Result<Void, Error>) -> Void) {
@@ -34,13 +33,66 @@ class ReportService: ObservableObject {
             }
         }
     }
+    
+    func fetchReports(completion: @escaping (Result<[Report], Error>) -> Void) {
+        reference.child(collectionName).observeSingleEvent(of: .value) { (snapshot: DataSnapshot) in
+            guard let reportDicts = snapshot.value as? [String: [String: Any]] else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "no report found."])))
+                return
+            }
+
+            var reports: [Report] = []
+            let dispatchGroup = DispatchGroup()
+
+            for (reportId, reportData) in reportDicts {
+                var report = Report(dictionary: reportData)
+                report.reportId = reportId
+
+                // Retrieve user details
+                let userId = report.reporterId
+
+                dispatchGroup.enter()
+                self.retrieveUserDetails(userId: userId) { userDetails, error in
+                    if let userDetails = userDetails {
+                        report.username = userDetails.username
+                        report.userprofilepicture = userDetails.profilepicture
+                    } else if let error = error {
+                        print("Error retrieving user details: \(error.localizedDescription)")
+                    }
+                    reports.append(report) // Add report to the list after setting user details
+                    dispatchGroup.leave()
+                }
+            }
+
+            // Notify after all user info is fetched
+            dispatchGroup.notify(queue: .main) {
+                completion(.success(reports))
+            }
+        } withCancel: { error in
+            completion(.failure(error)) // Handle cancellation error
+        }
+    }
+
+
+    func retrieveUserDetails(userId: String, completion: @escaping (Users?, Error?) -> Void) {
+        userService.getUserByID(uid: userId) { result in
+            switch result {
+            case .success(let user):
+                completion(user, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
+
+
     func fetchReportCount(completion: @escaping (Result<Int, Error>) -> Void) {
-           reference.child(collectionName).observeSingleEvent(of: .value) { snapshot in
-               let count = Int(snapshot.childrenCount)
-               self.reportCount = count // Update the published property
-               completion(.success(count))
-           } withCancel: { error in
-               completion(.failure(error))
-           }
-       }
-   }
+        reference.child(collectionName).observeSingleEvent(of: .value) { snapshot in
+            let count = Int(snapshot.childrenCount)
+           
+            completion(.success(count))
+        } withCancel: { error in
+            completion(.failure(error))
+        }
+    }
+}
